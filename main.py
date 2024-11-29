4df4a7c0076a0938d78c9eed76f4e18a1650e76a
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-29 23:37:06 +0000

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from deepface import DeepFace
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,6 +8,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/face")
+async def face():
+    objs = DeepFace.analyze(
+        img_path="img4.jpg",
+        actions=["age", "gender", "race", "emotion"],
+    )
+    return objs
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
